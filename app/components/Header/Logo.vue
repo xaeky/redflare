25d95833939d9770f22f58e8314c85fd1a7bbbf6
Xaeky <gh.sktd@protonmail.com>
2025-08-06 00:43:11 -0300

    vue: update non-prod badge

--- app/components/Header/Logo.vue
+++ app/components/Header/Logo.vue
@@ -6,8 +6,8 @@ const isProduction = runtime.public.redflare_env === 'production';
 </script>
 
 <template>
-  <div class="flex items-center gap-2 select-none">
+  <div class="flex items-center gap-1 select-none">
     <img :src="xaLogotypeSvg" class="h-12 inline-block" />
-    <span class="font-mono text-xs uppercase bg-warning-400 text-black py-1 px-2" v-if="!isProduction" v-text="runtime.public.redflare_env" />
+    <UBadge v-if="!isProduction" :label="runtime.public.redflare_env" variant="subtle" />
   </div>
 </template>
