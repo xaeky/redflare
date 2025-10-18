386290c1ed29dabeeff45decd583c6a4ccdcab3d
Xaeky <gh.sktd@protonmail.com>
2025-10-18 12:26:03 -0300

    api(fix): whitelist customer field

--- server/api/public/commissions/[commission_id].ts
+++ server/api/public/commissions/[commission_id].ts
@@ -4,9 +4,8 @@ export default defineEventHandler(async (event) => {
   const { data } = await validateCommission(event, true);
   if (!data) throw createError({ statusCode: 404, message: 'Commission not found' });
   // Whitelist public fields
-  const sanitizedWhitelistFields:Array<keyof typeof data> = ['_id', 'characters', 'created_at', 'updated_at', 'status', 'public_note', 'flags'];
+  const sanitizedWhitelistFields:Array<keyof typeof data> = ['_id', 'characters', 'customer', 'created_at', 'updated_at', 'status', 'public_note', 'flags'];
   const sanitizedPublicData = _.pick(data, sanitizedWhitelistFields);
   // Return commission data
-  console.log('sanitizedPublicData', sanitizedPublicData);
   return sanitizedPublicData;
 });
