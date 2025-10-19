ff283356a7ea54e1dfd8ba342a4b46cb815a03d8
Xaeky <gh.sktd@protonmail.com>
2025-10-19 12:13:48 -0300

    vue: move billing transaction form to a modal

--- app/components/Backoffice/Commission/Form/Billing.vue
+++ app/components/Backoffice/Commission/Form/Billing.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import * as z from 'zod';
-import type { WithId } from 'mongodb';
+import { BackofficeCommissionModalAddBillingTransaction } from '#components';
 
-const toast = useToast();
-
-const schema = commissionUpdateSchema;
-type Schema = z.output<typeof schema>;
-const state = defineModel<Schema>('state', {
-  default: {}
-});
-
+const overlay = useOverlay();
 const props = defineProps<{
   commission?: string
 }>();
+
+const openAddTransactionModal = () => {
+  const modalOverlay = overlay.create(BackofficeCommissionModalAddBillingTransaction, {
+    destroyOnClose: true
+  });
+  modalOverlay.open({
+    commission: props.commission,
+    onCreated: () => {
+      transactionsRefresh();
+      modalOverlay.close();
+    }
+  });
+};
 
 // Query remote commission's billing transactions
 const { data: transactions, isLoading: transactionsLoading, error, refetch: transactionsRefresh } = useQuery<Deserialized<PaymentTransaction>[]>({
@@ -22,98 +27,19 @@ const { data: transactions, isLoading: transactionsLoading, error, refetch: tran
   refetchOnWindowFocus: false
 });
 
-const now = new Date();
-const pad = (n: number) => n.toString().padStart(2, '0');
-const billingTransactionSchema = commissionPaymentOptionsSchema;
-const billingTransactionState = reactive<z.infer<typeof billingTransactionSchema>>({
-  total_paid_amount: 0,
-  net_received_amount: 0,
-  payment_currency: 'USD',
-  payment_processor: 'paypal',
-  payment_ext_id: '',
-  payment_ext_url: '',
-  approved_at: `${now.getFullYear()}-${pad(now.getMonth() + 1)}-${pad(now.getDate())}T${pad(now.getHours())}:${pad(now.getMinutes())}`,
-});
-const billingForm = {
-  paymentCurrencyOptions: [
-    { label: 'USD', value: 'USD' },
-    { label: 'ARS', value: 'ARS' },
-  ],
-  paymentProcessorOptions: [
-    { label: 'PayPal', value: 'paypal' },
-    { label: 'Mercado Pago', value: 'mercadopago' },
-  ]
-}
-const billingTransactionFormBusy = ref(false);
-const billingTransactionFormPostHandle = async () => {
-  try {
-    billingTransactionFormBusy.value = true;
-    await useAPI(`/api/commissions/${props.commission}/billing`, {
-      method: 'POST',
-      body: billingTransactionState
-    });
-    toast.add({
-      title: 'Transaction created',
-      description: 'The billing transaction has been created successfully.',
-      color: 'success'
-    });
-    transactionsRefresh();
-  } catch (error) {
-    toast.add({
-      title: 'Error creating transaction',
-      description: (error as Error).message,
-      color: 'error'
-    })
-  }
-  billingTransactionFormBusy.value = false;
-}
-const billingTransactionFormHide = ref(true);
 
-const formsTransactionCreate = ref();
 </script>
 
 <template>
   <div class="space-y-4">
-    <h1>Billing</h1>
-    <div>
-      <UButton color="neutral" variant="subtle" block @click="() => { billingTransactionFormHide = !billingTransactionFormHide }">
-        {{ billingTransactionFormHide ? 'Show' : 'Hide' }} transaction form
-      </UButton>
-    </div>
-    <div v-if="!billingTransactionFormHide" class="bg-muted p-4 rounded-lg">
-      <h2>Create transaction</h2>
-      <UForm ref="formsTransactionCreate" :schema="billingTransactionSchema" :state="billingTransactionState" @submit="billingTransactionFormPostHandle" class="space-y-4">
-        <div class="grid grid-cols-2 gap-4">
-          <UFormField label="Total paid amount" name="total_paid_amount" required>
-            <UInput type="number" v-model="billingTransactionState.total_paid_amount" />
-          </UFormField>
-          <UFormField label="Net received amount" name="net_received_amount" required>
-            <UInput type="number" v-model="billingTransactionState.net_received_amount" />
-          </UFormField>
-        </div>
-        <div class="grid grid-cols-2 gap-4">
-          <UFormField label="Payment currency" name="payment_currency" required>
-            <USelect v-model="billingTransactionState.payment_currency" :items="billingForm.paymentCurrencyOptions" class="w-full" />
-          </UFormField>
-          <UFormField label="Payment processor" name="payment_processor" required>
-            <USelect v-model="billingTransactionState.payment_processor" :items="billingForm.paymentProcessorOptions" class="w-full" />
-          </UFormField>
-        </div>
-        <UFormField label="Payment external URL" name="payment_ext_url">
-          <UInput v-model="billingTransactionState.payment_ext_url" class="w-full" />
-        </UFormField>
-        <div class="grid grid-cols-2 gap-4">
-          <UFormField label="Payment external ID" name="payment_ext_id" required>
-            <UInput v-model="billingTransactionState.payment_ext_id" class="w-full" />
-          </UFormField>
-          <UFormField label="Approved at" name="approved_at" required>
-            <UInput type="datetime-local" v-model="billingTransactionState.approved_at" class="w-full" />
-          </UFormField>
-        </div>
-        <div>
-          <UButton color="primary" type="submit" :loading="billingTransactionFormBusy" icon="i-lucide-plus">Create transaction</UButton>
-        </div>
-      </UForm>
+    <div class="flex items-center justify-between">
+      <h1>Billing</h1>
+      <div>
+        <UButton
+          @click="openAddTransactionModal"
+          label="Add transaction log" icon="i-heroicons-plus-16-solid"
+        />
+      </div>
     </div>
     <div class="space-y-2">
       <h2>Transactions</h2>
