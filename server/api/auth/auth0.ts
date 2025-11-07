dda145e7665f4a4153ff9cce5c95e151e6a8999e
Xaeky <gh.sktd@protonmail.com>
2025-11-07 16:19:17 -0300

    auth: add offline_access and email scopes to auth0

--- server/api/auth/auth0.ts
+++ server/api/auth/auth0.ts
@@ -1,6 +1,6 @@
 export default defineOAuthAuth0EventHandler({
   config: {
-    scope: ['openid', 'profile']
+    scope: ['openid', 'profile', 'offline_access', 'email']
   },
   async onSuccess(event, { user, tokens }) {
     const { access_token, refresh_token, id_token } = tokens;
