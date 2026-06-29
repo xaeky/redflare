5b5db8e950080d05761358fa2bc51bd43c976889
Xaeky <20586532+xaeky@users.noreply.github.com>
2026-06-28 23:02:22 -0300

    fix: force cold redirect for client side renders

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,4 +1,4 @@
 export default defineNuxtRouteMiddleware(() => {
   const { loggedIn } = useUserSession();
-  if (!loggedIn.value) return navigateTo('/api/auth/auth0');
+  if (!loggedIn.value) return navigateTo('/api/auth/auth0', { external: true });
 })
