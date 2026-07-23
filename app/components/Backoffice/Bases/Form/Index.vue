bec82c25d9bdd7beabdd633d834b02ed432edab9
Xaeky <20586532+xaeky@users.noreply.github.com>
2026-07-22 23:44:00 -0300

    fix(ui): remove formField container for avatarBase flags

--- app/components/Backoffice/Bases/Form/Index.vue
+++ app/components/Backoffice/Bases/Form/Index.vue
@@ -33,17 +33,12 @@ const isMeshEdit = flagsModel(AvatarBaseFlagsType.MeshEdit);
     <UFormField name="storefront_url" label="Storefront URL">
       <UInput v-model="avatarBaseFormStore.formState.storefront_url" class="w-full" />
     </UFormField>
-    <UFormField name="flags" label="Flags">
-      <div class="flex flex-col gap-2">
-        <UCheckbox v-model="isPrivate" label="Private" />
-        <UCheckbox v-model="isMeshKitbash" label="Kitbash" />
-        <UCheckbox v-model="isMeshEdit" label="Mesh Edit" />
+    <div>
+      <div class="space-y-2">
+        <UCheckbox v-model="isPrivate" label="Private" description="Exclude it from being displayed in the Public API." />
+        <UCheckbox v-model="isMeshKitbash" label="Kitbash" description="This base depends on another one." />
+        <UCheckbox v-model="isMeshEdit" label="Mesh Edit" description="This base has been edited from its original body mesh." />
       </div>
-      <UAlert color="neutral" variant="soft" class="mt-2" v-if="isPrivate" icon="i-heroicons-information-circle-16-solid">
-        <template #description>
-          Marking an avatar base as private will exclude it from being displayed in the Public API.
-        </template>
-      </UAlert>
-    </UFormField>
+    </div>
   </UForm>
 </template>
