1fa0cfcedfabdc52472504f130a21a229597a869
Xaeky <20586532+xaeky@users.noreply.github.com>
2026-07-25 14:56:18 -0300

    framework: remove unused middleware

--- app/middleware/to-landing.ts
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-  const { loggedIn } = useUserSession();
-  const runtimeConfig = useRuntimeConfig();
-  if (!loggedIn.value) return navigateTo(runtimeConfig.public.homepageUrl, { external: true });
-  else return navigateTo('/dashboard');
-})
