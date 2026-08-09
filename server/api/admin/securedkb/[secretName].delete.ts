7ec4cfd02a88f62d18ec452391d2802048b6b52f
Xaeky <20586532+xaeky@users.noreply.github.com>
2026-08-09 15:08:54 -0300

    feat: integrate Infisical SDK and endpoints

---
+++ server/api/admin/securedkb/[secretName].delete.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(async (event) => {
+  const { deleteSecret } = await useInfisical();
+  const { secretName } = getRouterParams(event);
+  if (!secretName) throw createError({ statusCode: 400, message: 'Missing secret name' });
+  const { environment } = getQuery(event);
+  await deleteSecret(secretName, environment?.toString() || undefined);
+});
---
+++ server/api/admin/securedkb/[secretName].put.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export default defineEventHandler(async (event) => {
+  const { setSecret } = await useInfisical();
+  const { secretName } = getRouterParams(event);
+  if (!secretName) throw createError({ statusCode: 400, message: 'Missing secret name' });
+  const { data, error } = await readValidatedBody(event, z.object({
+    secretValue: z.string().max(4096),
+    environment: z.string().optional()
+  }).safeParse);
+  if (!data || error) throw createError({ statusCode: 400, message: 'Invalid request body' });
+  const { secretValue, environment } = data;
+  await setSecret(secretName, secretValue, environment?.toString() || undefined);
+});
