a25a78a7613d6a4d50562a7c1b419600579fbdcf
Xaeky <20586532+xaeky@users.noreply.github.com>
2026-08-12 05:09:10 -0300

    refactor: integrate Infisical for Turnstile secret

--- server/utils/turnstile.ts
+++ server/utils/turnstile.ts
@@ -1,14 +1,20 @@
 import { z } from 'zod';
 import type { H3Event } from 'h3';
+import type { Secret } from '@infisical/sdk';
 
-export const verifyTurnstileToken = async (token: string, event: H3Event): Promise<TurnstileValidationResponse> => {
-  const { turnstile } = useRuntimeConfig(event);
+export const verifyTurnstileToken = async (token: string): Promise<TurnstileValidationResponse> => {
   if (!token) throw createError({ status: 400, message: 'Turnstile token is required.' });
-  if (!turnstile.secret_key) throw createError({ status: 500, message: 'Turnstile secret key is not configured.' });
+  let turnstileSecretKey: Secret;
+  try {
+    turnstileSecretKey = await (await useInfisical()).getSecret('TURNSTILE_SECRET_KEY');
+  } catch (error) {
+    logger.withTag('turnstile').error('Failed to retrieve Turnstile secret key from Infisical', error);
+    throw createError({ status: 500, message: 'Failed to retrieve Turnstile secret key.' });
+  }
   return await $fetch<TurnstileValidationResponse>('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
     method: 'POST',
     body: new URLSearchParams({
-      secret: turnstile.secret_key,
+      secret: turnstileSecretKey.secretValue,
       response: token,
     }),
     headers: {
@@ -20,7 +26,7 @@ export const verifyTurnstileToken = async (token: string, event: H3Event): Promi
 export const needTurnstileVerification = async (event: H3Event<EventHandlerRequest>) => {
   const { data, error } = await readValidatedBody(event, z.object({ turnstileToken: z.string() }).safeParse);
   if (!data || error) throw createError({ status: 400, message: 'Turnstile token is required.' });
-  const tokenResponse = await verifyTurnstileToken(data.turnstileToken, event);
+  const tokenResponse = await verifyTurnstileToken(data.turnstileToken);
   if (!tokenResponse.success) {
     logger.withTag('turnstile').error('Turnstile verification failed', tokenResponse['error-codes']);
     throw createError({ status: 403, message: 'Invalid Turnstile token.' });
