05cf14bcc0ac0cc2b306c9ee5d4682bfe0616906
Xaeky <20586532+xaeky@users.noreply.github.com>
2026-08-12 15:51:25 -0300

    tests: extend publicSession test to 10s

--- tests/e2e/publicSession.test.ts
+++ tests/e2e/publicSession.test.ts
@@ -2,6 +2,6 @@ import { test } from '@nuxt/test-utils/playwright';
 import { claimSession } from './utils/sessions';
 
 test('Claim public session in test environment', async ({ page }) => {
-  test.setTimeout(1000 * 3);
+  test.setTimeout(1000 * 10);
   await claimSession(page, 'public');
 });
