e268cddd115dfbaad61ef75486e343e946130189
Xaeky <20586532+xaeky@users.noreply.github.com>
2026-08-12 05:28:36 -0300

    refactor(tests): streamline commission fetching tests and remove redundancy

--- tests/e2e/frontofficeView.test.ts
+++ tests/e2e/frontofficeView.test.ts
@@ -3,30 +3,20 @@ import { claimSession } from './utils/sessions';
 
 test('Anon user is able to see any commission', async ({ page, goto, request }) => {
   test.setTimeout(1000 * 10);
-  const randomCommission = await request.get('/api/test/public/commissions/findAnyOne');
-  const randomCommissionId = (await randomCommission.json())._id;
-  await goto(`/commission/${randomCommissionId}`);
-  await page.waitForLoadState('networkidle');
-  await expect(page.getByRole('heading', { name: 'Commission' })).toBeVisible();
+  const randomCommissionResponse = await request.get('/api/test/public/commissions/findAnyOne');
+  const randomCommissionId = (await randomCommissionResponse.json())._id;
+  const res = await page.request.get(`/api/public/commissions/${randomCommissionId}`);
+  expect(res.ok()).toBe(true);
+  const rawData = await res.json();
+  expect(rawData.data).toBeDefined();
 });
 
 test('Public user is able to fetch their own commissions', async ({ page }) => {
   test.setTimeout(1000 * 10);
   await claimSession(page, 'public');
-  page.on('response', async (response) => {
-    if (response.url().includes('/api/commissions') && response.status() === 200) {
-      const rawData = await response.json();
-      expect(rawData.data).toBeDefined();
-      expect(Array.isArray(rawData.data)).toBe(true);
-    }
-  });
-});
-
-test('Public user is able to fetch their own commission', async ({ page, goto }) => {
-  test.setTimeout(1000 * 10);
-  await claimSession(page, 'public'); // On browser context
-  const randomOwnCommission = await page.request.get('/api/test/public/me/commissions/findAnyOne'); // On server(?) context
-  const randomOwnCommissionId = (await randomOwnCommission.json())._id;
-  await goto(`/commission/${randomOwnCommissionId}`);
-  await expect(page.getByRole('heading', { name: 'Commission' })).toBeVisible();
+  const res = await page.request.get('/api/public/me/commissions');
+  expect(res.ok()).toBe(true);
+  const rawData = await res.json();
+  expect(rawData.data).toBeDefined();
+  expect(Array.isArray(rawData.data)).toBe(true);
 });
